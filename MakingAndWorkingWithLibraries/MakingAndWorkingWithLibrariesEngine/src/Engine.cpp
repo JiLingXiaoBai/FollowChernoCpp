feadfb8ea96e78f558341078300adca8ba225e9c
Xiao Bai <uiojkl3588@qq.com>
2025-05-17 11:49:59 +0800

    MakingAndWorkingWithLibraries

---
+++ MakingAndWorkingWithLibraries/MakingAndWorkingWithLibrariesEngine/src/Engine.cpp
@@ -0,0 +1,8 @@
+#include "Engine.h"
+#include <iostream>
+
+namespace engine {
+	void PrintMessage() {
+		std::cout << "Hello World!" << std::endl;
+	}
+}
---
+++ MakingAndWorkingWithLibraries/MakingAndWorkingWithLibrariesGame/src/Application.cpp
@@ -0,0 +1,9 @@
+//#include "../../MakingAndWorkingWithLibrariesEngine/src/Engine.h"
+
+#include "Engine.h"
+#include <iostream>
+
+int main() {
+	engine::PrintMessage();
+	std::cin.get();
+}
